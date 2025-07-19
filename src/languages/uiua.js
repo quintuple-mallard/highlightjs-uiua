f9a978bbfffcbeec651c8d001213de6aa7da896e
J R M <jwrmmine@protonmail.com>
2025-07-19 11:00:02 +0200

    Create uiua.js

---
+++ src/languages/uiua.js
@@ -0,0 +1,10 @@
+/*
+Language: Uiua
+Description: A stack-based array programming language
+Website: https://uiua.org
+*/
+export default function(hljs) {
+  return {
+    name: 'Uiua',
+    keywords: ''
+}
